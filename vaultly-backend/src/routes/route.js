96d54930bd20c397219644990e389ec074ef7024
PinQusestion <mananbansal238@gmail.com>
2025-11-20 19:34:25 +0530

    Initialised backend

---
+++ vaultly-backend/src/routes/route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req,res) => {
+    res.send("Welcome to Vaultly!");
+})
+
+router.get("/dashboard", (req,res) => {
+    res.send("This is the dashboard.");
+})
+
+module.exports = router;
