a03a87964815921795e86febad45b41123316c47
PinQusestion <mananbansal238@gmail.com>
2025-12-02 11:15:14 +0530

    Fixed a bug

--- vaultly-backend/src/server.js
+++ vaultly-backend/src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'https://vaultly-one.vercel.app/'
+        'https://vaultly-one.vercel.app'
     ],
     credentials: true
 }));
