deb6ea60d12be8d73665661a0f8c4c15a24c8389
PinQusestion <mananbansal238@gmail.com>
2025-12-03 10:37:35 +0530

    fixed a bug

--- vaultly-backend/src/controllers/oauth.controller.js
+++ vaultly-backend/src/controllers/oauth.controller.js
@@ -63,7 +63,7 @@ const googleCallback = async (req, res) => {
     res.cookie('accessToken', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
